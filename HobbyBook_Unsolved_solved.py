b9299419c2fc607cf507c3b2d46a7ac26220ce64
dprenevost <dprenevost@yahoo.com>
2021-10-06 19:16:54 -0500

    Second commit

---
+++ HobbyBook_Unsolved_solved.py
@@ -0,0 +1,14 @@
+# @TODO: Your code here
+mydict = {"name": "Milo",
+        "age": 2,
+        "Hobby": ["sleeping", "eating", "farting"],
+        "Wakeup": {"Monday": 1, "Tuesday": 6, "Wednesday": 3}}
+
+print(f'Hello I am {mydict["name"]} and I am {mydict["age"]} years old.')
+print(f'I have {len(mydict["Hobby"])} hobbies...')
+print(f'My favorite hobby is {mydict["Hobby"][2]}')
+print(f'I love to wake Dad up Tuesdays at {mydict["Wakeup"]["Tuesday"]} AM! ')
+
+
+
+
